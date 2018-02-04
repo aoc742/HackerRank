ee5f4bca5c381a3e14a98f66d7b47aa509609993
Austin <atrain742@gmail.com>
2018-02-04 09:10:16 -0500

    setup + link to instructions

--- HackerRank/HackerlandRadioTransmiterrs/Program.cs
+++ HackerRank/HackerlandRadioTransmiterrs/Program.cs
@@ -8,8 +8,20 @@ namespace HackerlandRadioTransmiterrs
 {
     class Program
     {
+        static int hackerlandRadioTransmitters(int[] x, int k)
+        {
+            // Complete this function
+        }
+
         static void Main(string[] args)
         {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] x_temp = Console.ReadLine().Split(' ');
+            int[] x = Array.ConvertAll(x_temp, Int32.Parse);
+            int result = hackerlandRadioTransmitters(x, k);
+            Console.WriteLine(result);
         }
     }
 }
---
+++ HackerRank/HackerlandRadioTransmiterrs/README.md
@@ -0,0 +1,3 @@
+# Hackerland Radio Transmitters
+
+link: https://www.hackerrank.com/challenges/hackerland-radio-transmitters/problem
